ae5d07a01f7a20c33f2dd9791dac415258aa02f9
Nikita <powerboy20042006@gmail.com>
2023-08-03 17:29:05 +0300

    button fixed error

--- src/components/TaskCell.tsx
+++ src/components/TaskCell.tsx
@@ -1,11 +1,12 @@
 import { useAppDispatch } from "../redux/redux";
 import { setCurrentEdit, setIsEdit } from "../redux/slices/modalSlice";
 import { Item, deleteTask, setArchive } from "../redux/slices/tasksSlice";
-import * as React from "react";
 
 interface TasksCell {
   props: Item;
   isArchived: Boolean;
+  backgroundColor?: string;
+  size?: 'small' | 'medium' | 'large';
 }
 interface Categories {
   [key: string]: string;
@@ -17,7 +18,7 @@ export const categories: Categories = {
   'Idea': "idea.svg",
 };
 
-const TaskCell = ({ props, isArchived }: TasksCell) => {
+const TaskCell = ({ props, isArchived, backgroundColor, size = "medium" }: TasksCell) => {
   const dispatch = useAppDispatch();
 
   const onClickDeleteTask = () => {
@@ -28,12 +29,17 @@ const TaskCell = ({ props, isArchived }: TasksCell) => {
     dispatch(setCurrentEdit(props.id));
   };
 
-  React.useEffect(() => {}, []);
+  const sizes = {
+    "small" : "py-2",
+    "medium" : "py-3",
+    "large" : "py-4"
+  }
+
   return (
     <>
       {props.archived === isArchived && (
         <ul className="grid grid-cols-6">
-          <li className="flex items-center pl-4 bg-[#adb6e6]/[0.2] mb-3 py-3">
+          <li className={`flex items-center pl-4 bg-[#adb6e6]/[0.2] mb-3 ${sizes[size]}`} style={{ backgroundColor }}>
             <img
               className="w-10 mr-4"
               src={`/img/${categories[props.category]}`}
@@ -41,19 +47,20 @@ const TaskCell = ({ props, isArchived }: TasksCell) => {
             />
             {props.name}
           </li>
-          <li className="flex items-center bg-[#adb6e6]/[0.2] mb-3">{props.created}</li>
-          <li className="flex items-center bg-[#adb6e6]/[0.2] mb-3">{props.category}</li>
+          <li className="flex items-center bg-[#adb6e6]/[0.2] mb-3" style={{ backgroundColor }}>{props.created}</li>
+          <li className="flex items-center bg-[#adb6e6]/[0.2] mb-3" style={{ backgroundColor }}>{props.category}</li>
           <li
             id={String(props.id)}
             className="flex items-center bg-[#adb6e6]/[0.2] mb-3 cursor-zoom-in hover:font-semibold"
+            style={{ backgroundColor }}
             onClick={() => onClickEdit()}
           >
             {props.content.length > 30
               ? props.content.slice(0, 30) + "..."
               : props.content}
           </li>
-          <li className="flex items-center justify-center bg-[#adb6e6]/[0.2] mb-3">{props.date}</li>
-          <li className="flex justify-center bg-[#adb6e6]/[0.2] mb-3">
+          <li className="flex items-center justify-center bg-[#adb6e6]/[0.2] mb-3" style={{ backgroundColor }}>{props.date}</li>
+          <li className="flex justify-center bg-[#adb6e6]/[0.2] mb-3" style={{ backgroundColor }}>
             <img
               onClick={() => dispatch(setArchive(props.id))}
               className="w-6 mr-4 cursor-pointer"
